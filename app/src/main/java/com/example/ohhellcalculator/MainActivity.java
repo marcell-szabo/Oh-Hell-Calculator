af826f1f3c7535380f36e796b1702b9fee771e37
Marcell Szabo <szabomarcell.usa@gmail.com>
2020-06-15 23:03:55 +0200

    playernumber atadva

--- app/src/main/java/com/example/ohhellcalculator/MainActivity.java
+++ app/src/main/java/com/example/ohhellcalculator/MainActivity.java
@@ -5,16 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    Names n = new Names();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
     public void play(View view) {
-        Intent intent = new Intent(this, Names.class);
+        Intent intent = new Intent(this, n.getClass());
         startActivity(intent);
+        EditText etplayernum = (EditText) findViewById(R.id. editTextPlayerNum);
+        n.setPlayernumber(Integer.parseInt(etplayernum.getText().toString()));
     }
 }
--- app/src/main/java/com/example/ohhellcalculator/Names.java
+++ app/src/main/java/com/example/ohhellcalculator/Names.java
@@ -5,6 +5,11 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Names extends AppCompatActivity {
+    private int playernumber;
+
+    public void setPlayernumber(int playernumber) {
+        this.playernumber = playernumber;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
